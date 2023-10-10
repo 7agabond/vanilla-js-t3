03251292e33de43ea580a86ce12ce7def5ef93fd
Andres Garza <hanato21@icloud.com>
2023-10-10 17:58:43 -0500

    Chaged query selectors to simple function

--- js/view.js
+++ js/view.js
@@ -3,15 +3,15 @@ export default class View {
 
   constructor() {
     this.$.menu = this.qs('[data-id="menu"');
-    this.$.menuBtn = document.querySelector('[data-id="menu-btn"');
-    this.$.menuItems = document.querySelector('[data-id="menu-items"');
-    this.$.resetBtn = document.querySelector('[data-id="reset-btn"');
-    this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"');
+    this.$.menuBtn = this.qs('[data-id="menu-btn"');
+    this.$.menuItems = this.qs('[data-id="menu-items"');
+    this.$.resetBtn = this.qs('[data-id="reset-btn"');
+    this.$.newRoundBtn = this.qs('[data-id="new-round-btn"');
     this.$.squares = document.querySelectorAll('[data-id="square"]');
-    this.$.modal = document.querySelector('[data-id="modal"]');
-    this.$.modalText = document.querySelector('[data-id="modal-text"]');
-    this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
-    this.$.turn = document.querySelector('[data-id="turn"]');
+    this.$.modal = this.qs('[data-id="modal"]');
+    this.$.modalText = this.qs('[data-id="modal-text"]');
+    this.$.modalBtn = this.qs('[data-id="modal-btn"]');
+    this.$.turn = this.qs('[data-id="turn"]');
 
     //UI-only event listeners
     this.$.menuBtn.addEventListener("click", (event) => {
