8cb9cd72d6580139d6a33a020e87cdf76ac45702
Andres Garza <hanato21@icloud.com>
2023-10-12 15:08:15 -0500

    Working on the turn indicator method

--- js/view.js
+++ js/view.js
@@ -55,8 +55,18 @@ export default class View {
 
   // player = 1 | 2
   #setTurnIndicator(player) {
-    const icon = document.createElement('i')
-    const label = document.createElement('p')
+    const icon = document.createElement("i");
+    const label = document.createElement("p");
+
+    this.$.turn.classList.add(player === 1 ? "yellow" : "turquoise");
+    this.$.turn.classList.remove(player === 1 ? "turquoise" : "yellow");
+
+    icon.classList.add(player === 1 ? "fa-x" : "fa-o");
+
+    label.innerText =
+      player === 1 ? "Player 1, you're up!" : "Player 2, you're up!";
+
+    this.$.turn.replaceChildren(icon, label);
   }
 
   #qs(selector, parent) {
