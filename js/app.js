f7aed3778d0bb9536766edc1e38ffa237d72b733
Andres Garza <hanato21@icloud.com>
2023-10-13 17:52:28 -0500

    Fixed another bug

--- js/app.js
+++ js/app.js
@@ -180,7 +180,7 @@ function init() {
   });
 
   view.bindPlayerMoveEvent((event) => {
-    view.setTurnIndicator(players[1]);
+    view.setTurnIndicator(players[0]);
     view.handlePlayerMove(event.target, 1);
   });
 }
